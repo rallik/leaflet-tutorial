35a5d7cfd43a44b04dfb41043a513c078accd58d
Reuben T Allik <reubenallik@gmail.com>
2021-04-03 00:02:03 -0400

    functional map of boston

--- leaflet-tutorial/src/App.js
+++ leaflet-tutorial/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './App.scss'
+import 'leaflet/dist/leaflet.css';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({
+    lat: 42.3601,
+    long: -71.0589,
+    zoom: 15,
+    scrollZoom: false
+  });
+
+  const myIcon = L.icon({
+    iconUrl: './leaflet-tutorial/src/map-pin.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  });
+
+
+  const { zoom, lat, long, scrollZoom } = state;
+  const position = [lat, long];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <MapContainer className='map' center={position} zoom={zoom} scrollWheelZoom={scrollZoom}>
+        <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+          <Marker position={position} icon={myIcon}>
+            <Popup>
+              Popup
+            </Popup>
+          </Marker>
+      </MapContainer>
   );
 }
 
-export default App;
+export default App
