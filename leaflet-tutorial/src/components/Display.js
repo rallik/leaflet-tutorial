41162640f23d45db12ec6ba7fb5e2552855d2ece
Reuben T Allik <reubenallik@gmail.com>
2021-04-04 13:06:28 -0400

    done

--- leaflet-tutorial/src/components/Display.js
+++ leaflet-tutorial/src/components/Display.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import L from 'leaflet';
-import { MapContainer, TileLayer, WMSTileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import icon1 from '../assets/map-pin.png'
 
@@ -19,21 +19,9 @@ const Display = () => {
     popupAnchor: [-3, -76],
   });
   
-  let nexrad = {
-    url: "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi",
-    params: {
-      layers: 'nexrad-n0r-900913',
-      format: 'image/png',
-      transparent: true,
-      attribution: "Weather data © 2012 IEM Nexrad"
-    },
-    opacity: 0.5,
-};
-
   const { zoom, lat, long, scrollZoom } = state;
   const position = [lat, long];
 
-  console.log(<TileLayer {...nexrad} />)
   console.log(<TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />);
   
@@ -42,7 +30,6 @@ const Display = () => {
     <MapContainer className='map' center={position} zoom={zoom} scrollWheelZoom={scrollZoom}>
         <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <WMSTileLayer url={nexrad.url} params={nexrad.params} opacity={nexrad.opacity}/>
           <Marker position={position} icon={myIcon}>
             <Popup>
               Popup
